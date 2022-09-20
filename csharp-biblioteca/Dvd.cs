1f223eb8a051aa5ac9b8c89b6b416f3e4b0ce0b2
Hirisci <www.alan92@icloud.com>
2022-09-20 14:42:22 +0200

    Finito componente DVD

--- csharp-biblioteca/Dvd.cs
+++ csharp-biblioteca/Dvd.cs
@@ -4,6 +4,20 @@ namespace csharp_biblioteca
 {
     internal class Dvd : Product
     {
+        private int Duration { get; set; }
+
+        public Dvd(string title, int year, int sector, bool isLoaned, int rack, string author, int duration) : base(title, year, sector, isLoaned, rack, author)
+        {
+            Duration = duration;
+        }
         //mentre per i dvd la durata.
+
+        protected override string randomCode()
+        {
+            Random rnd = new Random();
+            return "333" + Convert.ToString(rnd.Next(1000)).PadLeft(10, '0');
+        }
+
+
     }
 }
